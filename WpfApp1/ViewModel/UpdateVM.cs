3f43eaa62228b67d4068b6a493bbea8bdb48a948
chethankumardev <chethan369@gmail.com>
2020-03-18 16:21:41 +0900

    initial commit

---
+++ WpfApp1/ViewModel/UpdateVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewModel
+{
+    public class UpdateVM 
+    {
+        //public UpdateVM()
+        //{
+        //}
+        //public Updater _updateCommand;
+        //public Updater UpdateCOmmand { get { if (_updateCommand == null) { _updateCommand = new Updater(UpdateCommand); } return _updateCommand; } }
+
+        //private void UpdateCommand()
+        //{
+        //   // WindowManager.ShowWindow(LAWindow.ProductInformationWindow, LAWindow.Main, WindowType.DialogWindow);
+        //}
+    }
+}
---
+++ WpfApp1/ViewModel/UserViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class UserViewModel
+    {
+        private IList<User> _UsersList;
+
+        public UserViewModel()
+        {
+            _UsersList = new List<User>
+            {
+                new User{UserId = 1,FirstName="Raj",LastName="Beniwal",City="Delhi",State="DEL",Country="INDIA"},
+                new User{UserId=2,FirstName="Mark",LastName="henry",City="New York", State="NY", Country="USA"},
+                new User{UserId=3,FirstName="Mahesh",LastName="Chand",City="Philadelphia", State="PHL", Country="USA"},
+                new User{UserId=4,FirstName="Vikash",LastName="Nanda",City="Noida", State="UP", Country="INDIA"},
+                new User{UserId=5,FirstName="Harsh",LastName="Kumar",City="Ghaziabad", State="UP", Country="INDIA"},
+                new User{UserId=6,FirstName="Reetesh",LastName="Tomar",City="Mumbai", State="MP", Country="INDIA"},
+                new User{UserId=7,FirstName="Deven",LastName="Verma",City="Palwal", State="HP", Country="INDIA"},
+                new User{UserId=8,FirstName="Ravi",LastName="Taneja",City="Delhi", State="DEL", Country="INDIA"}
+            };
+        }
+
+        public IList<User> Users
+        {
+            get { return _UsersList; }
+            set { _UsersList = value; }
+        }
+
+        private ICommand mUpdater;
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (mUpdater == null)
+                    mUpdater = new RelayCommand(UpdateCommands);
+                return mUpdater;
+            }
+            set
+            {
+                mUpdater = value;
+            }
+        }
+
+        public RelayCommand _updateCommand;
+        public RelayCommand UpdateCOmmand { get { if (_updateCommand == null) { _updateCommand = new RelayCommand(UpdateCommands); } return _updateCommand; } }
+
+        private void UpdateCommands()
+        {
+            // WindowManager.ShowWindow(LAWindow.ProductInformationWindow, LAWindow.Main, WindowType.DialogWindow);
+        }
+
+        public class RelayCommand : ICommand
+        {
+            private Action<object> execute;
+            private Func<object, bool> canExecute;
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+            {
+                this.execute = execute;
+                this.canExecute = canExecute;
+            }
+            public RelayCommand(Action actionToExecute, Func<object, bool> canExecute = null)
+            {
+                this.execute = execute;
+                this.canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return this.canExecute == null || this.canExecute(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                this.execute(parameter);
+            }
+        }
+    }
+}
